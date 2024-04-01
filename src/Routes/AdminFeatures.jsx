56436afe524dc10dd2ebd4f524abf4003484d844
Kike Gómez <kike.gomez.uy@gmail.com>
2024-04-01 17:52:11 -0300

    Update

--- src/Routes/AdminFeatures.jsx
+++ src/Routes/AdminFeatures.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../Components/Styles/AdminFeatures.css'
 import requestToAPI from '../services/requestToAPI';
 import IconButton from '../Components/IconButton';
